4fd8c7f3b20ff09c9abd34b3858c776aebd8ce02
Erik Poliak <erik@kire.sk>
2017-07-17 02:03:34 +0200

    cdn.kire.sk

--- sub/cdn/web/index.php
+++ sub/cdn/web/index.php
@@ -1,2 +1,14 @@
-<?php
-echo "CDN comming soon";
+<!DOCTYPE html>
+<html>
+    <link rel='shortcut icon' type='image/vnd.microsoft.icon' href='assets/img/ico/cdn16ico.ico'>
+    <link rel='apple-touch-icon' type='image/png' href='assets/img/png/cdn48png.png'>
+    <link rel='apple-touch-icon' type='image/png' sizes='72x72' href='assets/img/png/cdn72png.png'>
+    <link rel='apple-touch-icon' type='image/png' sizes='128x128' href='assets/img/png/cdn128png.png'>
+    <link rel='icon' type='image/png' href='assets/img/png/cdn128png.png'>
+
+    <link rel="stylesheet" type="text/css" href="assets/css/theme.css">
+    <title>cdn > kire</title>
+<body>
+<img class="logo" src="assets/img/png/cdn512png.png">
+</body>
+</html>
