e0f02c8ceee410707a554a8f0d911876e39925da
Erik Poliak <erik@kire.sk>
2017-07-23 15:40:33 +0200

    Recent comments - homepage

--- app/presenters/HomepagePresenter.php
+++ app/presenters/HomepagePresenter.php
@@ -4,12 +4,15 @@ namespace App\Presenters;
 
 
 use App\Model\Category;
+use App\Model\Comment;
 
 class HomepagePresenter extends BasePresenter {
 
     public function renderDefault() {
         $category = new Category($this->database);
+        $comment = new Comment($this->database);
         $this->template->categoryList = $category->getList();
         $this->template->newArticles = $category->getNewestArticles();
+        $this->template->recentComments = $comment->getComments_ByArticle()['comments'];
     }
 }
