c3d5b6cfde65f619d83b2d2903595b32cf5e996d
Erik Poliak <erik@kire.sk>
2017-07-23 12:32:41 +0200

    Comments under articles, basic

--- app/model/Comment.php
+++ app/model/Comment.php
@@ -17,7 +17,9 @@ class Comment {
         COLUMN_ARTICLE = 'Article',
         COLUMN_PARENT = 'Parent',
         COLUMN_CREATION_DATE = 'CreationDate',
-        COLUMN_TEXT = 'Text';
+        COLUMN_TEXT = 'Text',
+        COUNT_RECENT = 2,
+        COUNT_ANSWERS = 3;
 
 
     /** @var Nette\Database\Context */
@@ -27,4 +29,21 @@ class Comment {
         $this->database = $database;
     }
 
+    public function getComments_ByArticle(int $ArticleID, int $limit = self::COUNT_RECENT) {
+        $sel = $this->database->table(self::TABLE_NAME)->where([
+            self::COLUMN_ARTICLE => $ArticleID
+        ]);
+        $count = $sel->count();
+        $sel->where([
+            self::COLUMN_PARENT . ' IS NULL'
+        ])->limit($limit);
+        return ['count' => $count, 'comments' => $sel];
+    }
+
+    public function getAnswers(int $CommentID, $limit = self::COUNT_ANSWERS) {
+        $sel = $this->database->table(self::TABLE_NAME)->where([
+            self::COLUMN_PARENT => $CommentID
+        ])->limit($limit);
+        return $sel;
+    }
 }
--- app/presenters/ArticlePresenter.php
+++ app/presenters/ArticlePresenter.php
@@ -5,12 +5,14 @@ namespace App\Presenters;
 
 use App\Model\Article;
 use App\Model\Category;
+use App\Model\Comment;
 
 class ArticlePresenter extends BasePresenter {
 
     public function renderDefault($ID, $Name) {
         $category = new Category($this->database);
         $article = new Article($this->database);
+        $comment = new Comment($this->database);
         $this->template->categoryList = $category->getList();
 
         $article = $article->getArticle($ID);
@@ -20,5 +22,7 @@ class ArticlePresenter extends BasePresenter {
         }
         $this->template->article = $article;
         $this->template->newArticles = $category->getNewestArticles();
+        $this->template->comment = $comment;
+        $this->template->discussion = $comment->getComments_ByArticle($ID);
     }
 }
